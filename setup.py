1014886a4eced1448f24470d44346e52e09502cc
QMStar <qmstar0@gmail.com>
2022-12-21 13:14:25 +0800

    添加setup.py文件

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SimpleMail',
+    author='QMStar',
+    version='0.2.0',
+    author_email='qmstar0@gmail.com',
+    description='简易邮件发送脚本',
+    packages=[
+        # 'SimpleImage', 
+        'SimpleMail'
+    ]
+)
